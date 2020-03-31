2920298b11ddcd0bc363038b7070961da271d5fc
Carla Holcomb <carla.programmer@gmail.com>
2020-03-31 00:48:14 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   
   get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
   patch 'articles/:id', to: 'articles#update'
-  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+  resources :articles, only: [:index, :show, :new, :create]
   
 end
